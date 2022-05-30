d808a9dfb2feb9a736fd7e5f562bd61eceec573b
Costin Grigore <raisercostin@gmail.com>
2022-05-30 19:55:14 +0300

    fix compilation error

--- src/main/java/org/raisercostin/nodes/impl/GsonNodes.java
+++ src/main/java/org/raisercostin/nodes/impl/GsonNodes.java
@@ -11,6 +11,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParser;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 import io.vavr.control.Option;
 import org.raisercostin.nodes.Nodes;
